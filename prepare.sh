1c0c19a07567b2a234ae7089488c8e87ae571780
gbrlb <gbermudezar@gmail.com>
2023-08-21 16:37:18 -0300

    [REFACTORY] update run, and create prepare

---
+++ prepare.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+CONTAINER_NAME=locosim_jupyter
+CONTAINER_TAG=0.1
+CONTAINER_IMAGE=$CONTAINER_NAME:$CONTAINER_TAG
+
+# Host paths
+HOST_USER_HOME=$(pwd)/$USER
+HOST_WORKDIR=$HOST_USER_HOME/ros_ws/src
+HOST_LOCOSIMDIR=$HOST_WORKDIR/locosim
+
+# Container paths
+CONTAINER_USER_HOME=/home/$USER/ros_ws/src
+CONTAINER_WORKDIR=$CONTAINER_USER_HOME/ros_ws/src
+
+if [[ "$(docker images -q $CONTAINER_IMAGE 2> /dev/null)" == "" ]]; then
+	echo "Building $1 docker image..."
+	docker build -f Dockerfile -t $CONTAINER_IMAGE .
+fi
+
+# create HOST_WORKDIR
+mkdir -p $HOST_WORKDIR
+
+if [ ! -f "$HOST_USER_HOME/.bashrc" ]; then
+    echo "Copy .bashrc to $HOST_USER_HOME"
+    cp .bashrc $HOST_USER_HOME/.bashrc
+fi
+
+# Creating useful folders in host
+if [ ! -d "$HOST_LOCOSIMDIR" ]; then
+	echo "Cloning $HOST_LOCOSIMDIR"
+	git clone https://github.com/gbrlb/locosim.git -b jupyter --recursive $HOST_LOCOSIMDIR
+fi
--- run.sh
+++ run.sh
@@ -1,57 +1,19 @@
 #!/bin/bash
 
-CONTAINER_NAME=locosim_jupyter
-CONTAINER_TAG=1.0
-CONTAINER_IMAGE="$CONTAINER_NAME:$CONTAINER_TAG"
-# locosim_jupyter:1.0
-
-echo $CONTAINER_IMAGE
-if [[ "$(docker images -q $CONTAINER_IMAGE 2> /dev/null)" == "" ]]; then
-	echo "Building $1 docker image..."
-	docker build -f Dockerfile -t $CONTAINER_IMAGE .
-fi
-
-# Host paths
-HOST_USER_HOME=$(pwd)/$USER
-HOST_WORKDIR=$HOST_USER_HOME/ros_ws/src
-
-# Container paths
-CONTAINER_USER_HOME=/home/$USER
-CONTAINER_WORKDIR=$CONTAINER_USER_HOME/ros_ws/src
-CONTAINER_LOCOSIM=$HOST_WORKDIR/locosim
-
-# create HOST_WORKDIR
-mkdir -p $HOST_WORKDIR
-
-BASHRC=$HOST_USER_HOME/.bashrc
-if [ ! -f "$BASHRC" ]; then
-    echo "$BASHRC does not exist."
-    cp .bashrc $HOST_USER_HOME/.bashrc
-fi
-
-# Creating useful folders in host
-if [ ! -d "$CONTAINER_LOCOSIM" ]; then
-	echo "creating $CONTAINER_LOCOSIM"
-	git clone https://github.com/gbrlb/locosim.git -b jupyter --recursive $CONTAINER_LOCOSIM
-fi
-
 docker run --rm -it  \
-	-u $(id -u):$(id -g) \
-	-e USER  \
-	-e HOME \
-	-e SHELL \
-	-e "DOCKER=1"  \
-	-e "DISPLAY" \
-	-e "QT_X11_NO_MITSHM=1" \
+	--user=$(id -u):$(id -g) \
+	--env USER  \
+	--env HOME \
+	--env SHELL \
+	--env DOCKER=1  \
+	--env DISPLAY \
+	--env QT_X11_NO_MITSHM=1 \
 	--network=host \
-	--privileged  \
-	--volume="/etc/group:/etc/group:ro"   \
-	--volume="/etc/passwd:/etc/passwd:ro" \
-	--volume="/etc/shadow:/etc/shadow:ro"  \
-	--volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
-	--volume $HOST_USER_HOME:$HOME:rw \
-	--device=/dev/dri:/dev/dri  \
-	--workdir="$CONTAINER_WORKDIR" \
-	--name="$CONTAINER_NAME" \
+	--volume /etc/group:/etc/group:ro   \
+	--volume /etc/passwd:/etc/passwd:ro \
+	--volume /etc/shadow:/etc/shadow:ro  \
+	--volume /etc/sudoers.d:/etc/sudoers.d:ro \
+	--volume $(pwd)/$USER:$HOME:rw \
 	--entrypoint bash \
-	$CONTAINER_IMAGE
+	--name locosim_jupyter \
+	locosim_jupyter:0.1
