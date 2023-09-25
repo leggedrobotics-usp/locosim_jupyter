edc183d8c3d7592172ae513bfa90d020fae696e1
gbrlb <gbermudezar@gmail.com>
2023-09-25 12:37:20 -0300

    uncomment build

--- prepare.sh
+++ prepare.sh
@@ -10,15 +10,12 @@ HOST_WORKDIR=$HOST_USER_HOME/ros_ws/src
 HOST_LOCOSIMDIR=$HOST_WORKDIR/locosim
 
 # Container paths
-CONTAINER_USER_HOME=/home/$USER/ros_ws/src
-CONTAINER_WORKDIR=$CONTAINER_USER_HOME/ros_ws/src
+if [[ "$(docker images -q $CONTAINER_IMAGE 2> /dev/null)" == "" ]]; then
+	echo "Building $1 docker image..."
+	docker build -f Dockerfile -t $CONTAINER_IMAGE . --progress=plain
+fi
 
-# if [[ "$(docker images -q $CONTAINER_IMAGE 2> /dev/null)" == "" ]]; then
-# 	echo "Building $1 docker image..."
-# 	docker build -f Dockerfile -t $CONTAINER_IMAGE . --progress=plain
-# fi
-
-# # create HOST_WORKDIR
+# create HOST_WORKDIR
 mkdir -p "$HOST_WORKDIR"
 
 if [ ! -f "$HOST_USER_HOME/.bashrc" ]; then
