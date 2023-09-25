b36da99b7593b2d38795fd95b756d1c036b3cc30
gbrlb <gbermudezar@gmail.com>
2023-09-25 13:17:35 -0300

    add "

--- run_linux.sh
+++ run_linux.sh
@@ -9,11 +9,11 @@ docker run --rm -it  \
 	--env DISPLAY \
 	--env QT_X11_NO_MITSHM=1 \
 	-p 8888:8888 \
-	--volume /etc/group:/etc/group:ro   \
-	--volume /etc/passwd:/etc/passwd:ro \
-	--volume /etc/shadow:/etc/shadow:ro  \
-	--volume /etc/sudoers.d:/etc/sudoers.d:ro \
-	--volume /tmp/.X11-unix:/tmp/.X11-unix \
+	--volume "/etc/group:/etc/group:ro" \
+	--volume "/etc/passwd:/etc/passwd:ro" \
+	--volume "/etc/shadow:/etc/shadow:ro" \
+	--volume "/etc/sudoers.d:/etc/sudoers.d:ro" \
+	--volume "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 	--volume "$(pwd)/$USER:$HOME:rw" \
 	--entrypoint bash \
 	--name locosim_jupyter \
