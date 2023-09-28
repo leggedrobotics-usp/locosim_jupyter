80f81f4c9139776f48cc46295457a2fe94970a27
gbrlb <gbermudezar@gmail.com>
2023-09-28 11:25:43 -0300

    update run_wsl

--- run_wsl.sh
+++ run_wsl.sh
@@ -9,13 +9,13 @@ docker run --rm -it  \
 	--env DISPLAY \
 	--env QT_X11_NO_MITSHM=1 \
 	-p 8888:8888 \
-	--volume /etc/group:/etc/group:ro   \
-	--volume /etc/passwd:/etc/passwd:ro \
-	--volume /etc/shadow:/etc/shadow:ro  \
-	--volume /etc/sudoers.d:/etc/sudoers.d:ro \
-	--volume /tmp/.X11-unix:/tmp/.X11-unix \
-	--volume /mnt/wslg:/mnt/wslg \
-	--volume $(pwd)/$USER:$HOME:rw \
+	--volume "/etc/group:/etc/group:ro" \
+	--volume "/etc/passwd:/etc/passwd:ro" \
+	--volume "/etc/shadow:/etc/shadow:ro" \
+	--volume "/etc/sudoers.d:/etc/sudoers.d:ro" \
+	--volume "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	--volume "$(pwd)/$USER:$HOME:rw" \
+	--volume "/mnt/wslg:/mnt/wslg" \
 	--privileged \
 	--entrypoint bash \
 	--name locosim_jupyter \
